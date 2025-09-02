555018d62b227c5a15624b6b9b5b5d54bafbd32b
srishylam burla <srishylamburla1@gmail.com>
2025-09-02 17:00:23 +0530

    production

--- portfolio/src/redux/api/subscribeApi.js
+++ portfolio/src/redux/api/subscribeApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const subscribeApi = createApi({
   reducerPath: "subscribeApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://srishylam-portfolio-1.onrender.com/api",
+    baseUrl: "http://localhost:5000/api",
     // credentials: "include",
   }),
   endpoints: (builder) => ({
